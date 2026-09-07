n08i40k/exteragram-plugin-template

feat: use `extera-plugin` gradle plugin

build.gradle.kts
@@ -1,7 +1,4 @@
-import com.github.jengelman.gradle.plugins.shadow.relocation.SimpleRelocator
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
 
 val minSdkMajorProperty: Provider<Int> =
     providers.gradleProperty("minSdkMajor").map { it?.toInt() }
@@ -11,13 +8,6 @@ val targetSdkMajorProperty: Provider<Int> =
 
 val targetSdkMinorProperty: Provider<Int> =
     providers.gradleProperty("targetSdkMinor").map { it?.toInt() }
-
-val telegramJarPathProperty: Provider<String> =
-    providers.gradleProperty("telegramJarPath")
-
-val dexOutputDirProperty: Provider<Directory> =
-    providers.gradleProperty("dexOutputDir")
-        .map { layout.projectDirectory.dir(it) }
 
 buildscript {
     repositories {
@@ -31,6 +21,8 @@ plugins {
 
     // Translations.
     id("de.comahe.i18n4k") version "0.11.2"
+
+    id("io.github.n08i40k.extera")
 }
 
 i18n4k {
@@ -89,15 +81,6 @@ kotlin {
 }
 
 dependencies {
-    // Telegram itself.
-    compileOnly(files(telegramJarPathProperty.map { path ->
-        file(path).also {
-            require(it.isFile) {
-                "$path is missing, run `just strip-telegram-jar` to generate it from libs/Telegram.jar"
-            }
-        }
-    }))
-
     // Kotlin.
     implementation(libs.jetbrains.kotlin.stdlib)
 
@@ -121,101 +104,40 @@ dependencies {
     coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
 
-/**
- * Get list of .jar files in provided configuration.
- * artifactView is required for getting .jar files from .aar libraries.
- */
-fun classesJarsOf(configurationName: String): Provider<FileCollection> =
-    configurations.named(configurationName).map { configuration ->
-        configuration.incoming
-            .artifactView {
-                attributes.attribute(
-                    Attribute.of("artifactType", String::class.java),
-                    "android-classes-jar"
-                )
-                lenient(true)
-            }
-            .files
+exteraPlugin {
+    pluginId = "exteragram-plugin-template"
+    pluginName = "exteraGram plugin template"
+    pluginDescription = "A template of exteraGram plugin with embedded DEX"
+    pluginAuthor = "@n08i40k_extera"
+    pluginVersion = "0.0.0"
+
+    minClientVersion = "12.1.1"
+
+    entryClass = "ru.n08i40k.template.Plugin"
+
+    telegramJar = file("libs/Telegram.jar")
+    conflictingPackages = listOf("kotlin")
+
+    proguardFiles = files("proguard-rules.pro")
+    minSdk.set(minSdkMajorProperty)
+
+    shadedPackage = "ru.n08i40k.template_shaded"
+
+    // kotlin
+    relocate("kotlin", "kotlinx")
+
+    // i18n4k
+    relocate("de.comahe.i18n4k")
+
+    relocate("androidx") {
+        // An example of excluding package/class from remapping
+        // to keep compatibility with host functions, etc.
+        exclude("androidx.collection.LongSparseArray")
+        exclude("androidx.core.view.inputmethod.InputContentInfoCompat")
+
+        // compileOnly packages are resolved from the host at runtime,
+        // so references to them must stay non-relocated.
+        exclude("androidx.recyclerview.**")
+        exclude("androidx.lifecycle.**")
     }
-
-androidComponents {
-    onVariants { variant ->
-        val variantName = variant.name
-        val variantTitle = variantName.replaceFirstChar { it.uppercase() }
-
-        val runtimeJars = classesJarsOf("${variantName}RuntimeClasspath")
-        val compileJars = classesJarsOf("${variantName}CompileClasspath")
-
-        fun buildDir(child: String): Provider<Directory> =
-            layout.buildDirectory.dir(child)
-
-        fun buildFile(child: String): Provider<RegularFile> =
-            layout.buildDirectory.file(child)
-
-        val shadedJar = tasks.register<ShadowJar>("shade$variantTitle") {
-            destinationDirectory.set(buildDir("intermediates/shaded"))
-            archiveFileName.set("classes-${variantName}.jar")
-
-            from(
-                tasks.named<KotlinCompileTool>("compile${variantTitle}Kotlin")
-                    .flatMap { it.destinationDirectory })
-            from(
-                tasks.named<JavaCompile>("compile${variantTitle}JavaWithJavac")
-                    .flatMap { it.destinationDirectory })
-            from(runtimeJars.map { jars -> jars.map(::zipTree) })
-
-            /**
-             * Simple shorthand function that relocate provided packages into fixed parent.
-             */
-            fun rel(pkg: String, action: Action<SimpleRelocator> = Action {}) {
-                val pkg = pkg.trimEnd('.') + "."
-                return relocate(pkg, "ru.n08i40k.template_shaded.$pkg", action)
-            }
-
-            rel("kotlin")
-            rel("kotlinx")
-            rel("de.comahe.i18n4k")
-
-            rel("androidx") {
-                // An example of excluding package/class from remapping
-                // to keep compatibility with host functions, etc.
-                exclude("androidx.collection.LongSparseArray")
-                exclude("androidx.core.view.inputmethod.InputContentInfoCompat")
-
-                // compileOnly packages are resolved from the host at runtime,
-                // so references to them must stay non-relocated.
-                exclude("androidx.recyclerview.**")
-                exclude("androidx.lifecycle.**")
-            }
-        }
-
-        tasks.register<BuildDexTask>("buildDex$variantTitle") {
-            group = "build"
-
-            programJars.from(shadedJar)
-
-            bootClasspathJars.from(sdkComponents.bootClasspath)
-
-            classpathJars.from(
-                compileJars.map { it.minus(runtimeJars.get()) },
-                telegramJarPathProperty.map(::file)
-            )
-
-            proguardFiles.from(file("proguard-rules.pro"))
-
-            minSdk.set(minSdkMajorProperty)
-
-            release.set(variant.buildType == "release")
-
-            mergedClasspathJar.set(buildFile("intermediates/dex-classpath/${variantName}/classpath.jar"))
-
-            // set output dir to dist folder
-            outputDir.set(dexOutputDirProperty.map { it.dir(variantName) })
-        }
-    }
-}
-
-tasks.register("buildDex") {
-    group = "build"
-    dependsOn(tasks.withType<BuildDexTask>())
 }

exteraStuff/pydex-plugin-template

build.gradle.kts
@@ -1,7 +1,4 @@
-import com.github.jengelman.gradle.plugins.shadow.relocation.SimpleRelocator
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
 
 val minSdkMajorProperty: Provider<Int> =
     providers.gradleProperty("minSdkMajor").map { it?.toInt() }
@@ -11,13 +8,6 @@ val targetSdkMajorProperty: Provider<Int> =
 
 val targetSdkMinorProperty: Provider<Int> =
     providers.gradleProperty("targetSdkMinor").map { it?.toInt() }
-
-val telegramJarPathProperty: Provider<String> =
-    providers.gradleProperty("telegramJarPath")
-
-val dexOutputDirProperty: Provider<Directory> =
-    providers.gradleProperty("dexOutputDir")
-        .map { layout.projectDirectory.dir(it) }
 
 buildscript {
     repositories {
@@ -31,6 +21,8 @@ plugins {
 
     // Translations.
     id("de.comahe.i18n4k") version "0.11.2"
+
+    id("io.github.n08i40k.extera")
 }
 
 i18n4k {
@@ -89,15 +81,6 @@ kotlin {
 }
 
 dependencies {
-    // Telegram itself.
-    compileOnly(files(telegramJarPathProperty.map { path ->
-        file(path).also {
-            require(it.isFile) {
-                "$path is missing, run `just strip-telegram-jar` to generate it from libs/Telegram.jar"
-            }
-        }
-    }))
-
     // Kotlin.
     implementation(libs.jetbrains.kotlin.stdlib)
 
@@ -121,101 +104,48 @@ dependencies {
     coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
 
-/**
- * Get list of .jar files in provided configuration.
- * artifactView is required for getting .jar files from .aar libraries.
- */
-fun classesJarsOf(configurationName: String): Provider<FileCollection> =
-    configurations.named(configurationName).map { configuration ->
-        configuration.incoming
-            .artifactView {
-                attributes.attribute(
-                    Attribute.of("artifactType", String::class.java),
-                    "android-classes-jar"
-                )
-                lenient(true)
-            }
-            .files
+extera {
+    telegram {
+        jar = file("libs/Telegram.jar")
+        conflictingPackages = listOf("kotlin")
     }
 
-androidComponents {
-    onVariants { variant ->
-        val variantName = variant.name
-        val variantTitle = variantName.replaceFirstChar { it.uppercase() }
+    r8 {
+        proguardFiles = files("proguard-rules.pro")
+        minSdk.set(minSdkMajorProperty)
+    }
 
-        val runtimeJars = classesJarsOf("${variantName}RuntimeClasspath")
-        val compileJars = classesJarsOf("${variantName}CompileClasspath")
+    shadow {
+        targetPackage = "ru.n08i40k.template_shaded"
 
-        fun buildDir(child: String): Provider<Directory> =
-            layout.buildDirectory.dir(child)
+        // kotlin
+        relocate("kotlin", "kotlinx")
 
-        fun buildFile(child: String): Provider<RegularFile> =
-            layout.buildDirectory.file(child)
+        // i18n4k
+        relocate("de.comahe.i18n4k")
 
-        val shadedJar = tasks.register<ShadowJar>("shade$variantTitle") {
-            destinationDirectory.set(buildDir("intermediates/shaded"))
-            archiveFileName.set("classes-${variantName}.jar")
+        relocate("androidx") {
+            // An example of excluding package/class from remapping
+            // to keep compatibility with host functions, etc.
+            exclude("androidx.collection.LongSparseArray")
+            exclude("androidx.core.view.inputmethod.InputContentInfoCompat")
 
-            from(
-                tasks.named<KotlinCompileTool>("compile${variantTitle}Kotlin")
-                    .flatMap { it.destinationDirectory })
-            from(
-                tasks.named<JavaCompile>("compile${variantTitle}JavaWithJavac")
-                    .flatMap { it.destinationDirectory })
-            from(runtimeJars.map { jars -> jars.map(::zipTree) })
-
-            /**
-             * Simple shorthand function that relocate provided packages into fixed parent.
-             */
-            fun rel(pkg: String, action: Action<SimpleRelocator> = Action {}) {
-                val pkg = pkg.trimEnd('.') + "."
-                return relocate(pkg, "ru.n08i40k.template_shaded.$pkg", action)
-            }
-
-            rel("kotlin")
-            rel("kotlinx")
-            rel("de.comahe.i18n4k")
-
-            rel("androidx") {
-                // An example of excluding package/class from remapping
-                // to keep compatibility with host functions, etc.
-                exclude("androidx.collection.LongSparseArray")
-                exclude("androidx.core.view.inputmethod.InputContentInfoCompat")
-
-                // compileOnly packages are resolved from the host at runtime,
-                // so references to them must stay non-relocated.
-                exclude("androidx.recyclerview.**")
-                exclude("androidx.lifecycle.**")
-            }
-        }
-
-        tasks.register<BuildDexTask>("buildDex$variantTitle") {
-            group = "build"
-
-            programJars.from(shadedJar)
-
-            bootClasspathJars.from(sdkComponents.bootClasspath)
-
-            classpathJars.from(
-                compileJars.map { it.minus(runtimeJars.get()) },
-                telegramJarPathProperty.map(::file)
-            )
-
-            proguardFiles.from(file("proguard-rules.pro"))
-
-            minSdk.set(minSdkMajorProperty)
-
-            release.set(variant.buildType == "release")
-
-            mergedClasspathJar.set(buildFile("intermediates/dex-classpath/${variantName}/classpath.jar"))
-
-            // set output dir to dist folder
-            outputDir.set(dexOutputDirProperty.map { it.dir(variantName) })
+            // compileOnly packages are resolved from the host at runtime,
+            // so references to them must stay non-relocated.
+            exclude("androidx.recyclerview.**")
+            exclude("androidx.lifecycle.**")
         }
     }
-}
 
-tasks.register("buildDex") {
-    group = "build"
-    dependsOn(tasks.withType<BuildDexTask>())
+    manifest {
+        id = "exteragram-plugin-template"
+        name = "exteraGram plugin template"
+        description = "A template of exteraGram plugin with embedded DEX"
+        author = "@n08i40k_extera"
+        version = "0.0.0"
+
+        minClientVersion = "12.1.1"
+
+        entryClass = "ru.n08i40k.template.Plugin"
+    }
 }